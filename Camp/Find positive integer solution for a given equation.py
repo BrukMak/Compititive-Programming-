c1b265ea8281b735f73621eda663cec552052c82
BrukMak <60716114+BrukMak@users.noreply.github.com>
2022-02-25 11:02:58 +0300

    Create Find positive integer solution for a given equation.py

---
+++ Camp/Find positive integer solution for a given equation.py
@@ -0,0 +1,32 @@
+"""
+   This is the custom function interface.
+   You should not implement it, or speculate about its implementation
+   class CustomFunction:
+       # Returns f(x, y) for any given positive integers x and y.
+       # Note that f(x, y) is increasing with respect to both x and y.
+       # i.e. f(x, y) < f(x + 1, y), f(x, y) < f(x, y + 1)
+       def f(self, x, y):
+  
+"""
+
+class Solution:
+    def findSolution(self, customfunction: 'CustomFunction', z: int) -> List[List[int]]:
+        result = []
+        for x in range(1,z +1):
+            l = 1
+            r = z
+            while l <= r:
+                mid = l + (r-l)//2
+                
+                val = customfunction.f(x,mid)
+                
+                if val == z:
+                    result.append([x,mid])
+                    break
+                if val < z:
+                    l = mid + 1
+                else:
+                    r = mid -1
+        return result
+    
+        
