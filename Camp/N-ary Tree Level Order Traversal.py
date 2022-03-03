fa32dc946248dcb8135c935569a6f13b28f092af
BrukMak <60716114+BrukMak@users.noreply.github.com>
2022-03-03 18:10:53 +0300

    Create N-ary Tree Level Order Traversal.py

---
+++ Camp/N-ary Tree Level Order Traversal.py
@@ -0,0 +1,34 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+from collections import deque
+class Solution:
+    def levelOrder(self, root: 'Node') -> List[List[int]]:
+        if not root:
+            return []
+        
+        queue = deque()
+        ans = [[root.val]]
+        queue.append(root)
+        
+        while queue:
+            
+            size = len(queue)
+            temp = []
+            for _ in range(size):
+                cur = queue.popleft()
+                if cur.children:
+                    for child in cur.children:
+                        temp.append(child.val)
+                        queue.append(child)
+            if temp:
+                ans.append(temp)
+            
+        return ans
+                
+                 
+                
