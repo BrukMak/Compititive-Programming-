a2e87274430b74e3ff9cd204b731a86dac312ae6
BrukMak <60716114+BrukMak@users.noreply.github.com>
2022-03-10 12:03:34 +0300

    Time: 1818 ms (20.62%), Space: 140.9 MB (6.28%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
@@ -1,13 +1,13 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         
-        ans = 0
-        prev_min = float("inf")
-        for i in prices:
-            ans = max(ans, i - prev_min )
-            if i < prev_min:
-                prev_min = i
-            
-        return ans
-    
-            
+        result = 0
+        def dfs(ind):
+            nonlocal result
+            if ind == len(prices)-1:
+                return prices[ind]
+            max_after = dfs(ind+1)
+            result = max(result , max_after - prices[ind])
+            return max(max_after, prices[ind])
+        dfs(0)
+        return result
