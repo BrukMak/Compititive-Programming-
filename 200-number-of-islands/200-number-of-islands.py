c9805a292ba7fd054b227b070b60de69d5ba02e6
BrukMak <60716114+BrukMak@users.noreply.github.com>
2022-03-20 20:57:13 +0300

    Time: 613 ms (12.99%), Space: 16.5 MB (50.63%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -2,14 +2,19 @@ class Solution:
     def inbound(self, r ,c):
         return 0 <= r < self.m and 0 <= c < self.n
     def dfs(self, row, col, grid):
-        
+        Dir = [(1, 0), (-1, 0), (0, 1), (0, -1)]
         if grid[row][col] == "1":
             grid[row][col] = "v"
             
-            if self.inbound(row - 1, col) and grid[row - 1][col]:self.dfs(row - 1, col, grid)
-            if self.inbound(row + 1, col) and grid[row + 1][col]:self.dfs(row + 1, col, grid)
-            if self.inbound(row, col - 1) and grid[row][col - 1]:self.dfs(row, col - 1, grid)
-            if self.inbound(row, col + 1) and grid[row][col + 1]:self.dfs(row, col + 1, grid)
+            # if self.inbound(row - 1, col) and grid[row - 1][col]:self.dfs(row - 1, col, grid)
+            # if self.inbound(row + 1, col) and grid[row + 1][col]:self.dfs(row + 1, col, grid)
+            # if self.inbound(row, col - 1) and grid[row][col - 1]:self.dfs(row, col - 1, grid)
+            # if self.inbound(row, col + 1) and grid[row][col + 1]:self.dfs(row, col + 1, grid)
+            
+            for d in Dir:
+                if self.inbound(row + d[0] , col + d[1]) and grid[row + d[0]][col + d[1]]:
+                    self.dfs(row + d[0] , col + d[1], grid)
+                
             
     def numIslands(self, grid: List[List[str]]) -> int:
         self.m = len(grid)
