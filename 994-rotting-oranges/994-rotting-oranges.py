c42f7f978c4bebb99b77497b31dbf5e019f08bf0
BrukMak <60716114+BrukMak@users.noreply.github.com>
2022-03-20 21:09:50 +0300

    Time: 95 ms (22.50%), Space: 14 MB (58.50%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.py
+++ 994-rotting-oranges/994-rotting-oranges.py
@@ -3,6 +3,7 @@ class Solution:
     def inbound(self, row, col):
         return 0 <= row < self.rows and 0 <= col < self.cols
     def orangesRotting(self, grid: List[List[int]]) -> int:
+        Dir = [(1, 0), (-1, 0), (0, 1), (0, -1)]
         self.rows = len(grid)
         self.cols = len(grid[0])
         count = 0
@@ -15,20 +16,13 @@ class Solution:
             size = len(queue)
             for _ in range(size):
                 r, c = queue.popleft()
-                if self.inbound(r - 1, c) and grid[r - 1][c] == 1:
-                    grid[r - 1][c] = 2
-                    queue.append((r - 1 , c))
-                if self.inbound(r + 1, c) and grid[r + 1][c] == 1:
-                    grid[r + 1][c] = 2
-                    queue.append((r + 1 , c))
-                if self.inbound(r, c - 1) and grid[r][c - 1] == 1:
-                    grid[r][c - 1] = 2
-                    queue.append((r , c - 1))
-                if self.inbound(r, c + 1) and grid[r][c + 1] == 1:
-                    grid[r][c + 1] = 2
-                    queue.append((r , c + 1))
+                
+                for d in Dir:
+                    if self.inbound(r + d[0], c + d[1]) and grid[r + d[0]][c + d[1]] == 1:
+                        grid[r + d[0]][c + d[1]] = 2
+                        queue.append((r+d[0], c + d[1]))
             count += 1
-        # print(grid)
+            
         for row in range(self.rows):
             for col in range(self.cols):
                 if grid[row][col] == 1:
