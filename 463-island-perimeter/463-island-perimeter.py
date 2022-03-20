3adf3852ed6124c09df385896e25557f09f12858
BrukMak <60716114+BrukMak@users.noreply.github.com>
2022-03-20 20:48:32 +0300

    Time: 913 ms (26.17%), Space: 24.3 MB (11.41%) - LeetHub

---
+++ 463-island-perimeter/463-island-perimeter.py
@@ -0,0 +1,29 @@
+class Solution:
+    def islandPerimeter(self, grid: List[List[int]]) -> int:
+        rows = len(grid)
+        cols = len(grid[0])
+        visited = set()
+        perimeter = 0
+        def dfs(row, col):
+            nonlocal perimeter
+            if (row, col) in visited:
+                return
+            
+            Dir = [[0,1], [0, -1], [1, 0], [-1, 0]]
+            
+            if row < 0 or row >= rows or col < 0 or col >= cols or grid[row][col] == 0:
+                perimeter += 1
+            
+            else:
+                visited.add((row, col))
+                for d in Dir:
+                    
+                    dfs(row + d[0], col + d[1])
+                    
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == 1:
+                    dfs(r, c)
+                    
+        return perimeter
+            
