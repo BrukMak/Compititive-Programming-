afad964257002fce07c80fab56f2776d0dca3fc7
BrukMak <60716114+BrukMak@users.noreply.github.com>
2022-03-21 13:40:47 +0300

    Time: 783 ms (5.04%), Space: 13.9 MB (78.97%) - LeetHub

---
+++ 763-partition-labels/763-partition-labels.py
@@ -0,0 +1,28 @@
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        r = l = sp = 0
+        # visited = set()
+        ans = []
+        while r < len(s) :
+            for i in range(r + 1, len(s)):
+                if s[sp] == s[i]:
+                    r = i
+            if r == l:
+                ans.append(1)
+                r += 1
+                l += 1
+                sp = l
+                continue
+            elif r == len(s) - 1:
+                ans.append(r - l + 1)
+                break
+                
+            elif sp == r:
+                ans.append(r - l + 1)
+                sp += 1
+                r += 1
+                l = r 
+                continue
+            sp += 1
+        return ans
+        
