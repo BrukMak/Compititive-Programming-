0592fb8abdf50aa62a921f1858c01b0ca95a760f
BrukMak <60716114+BrukMak@users.noreply.github.com>
2022-03-26 10:20:49 +0300

    Time: 70 ms (30.39%), Space: 13.9 MB (43.95%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.py
+++ 1029-two-city-scheduling/1029-two-city-scheduling.py
@@ -3,11 +3,13 @@ class Solution:
         prof = []
         tot_sum = 0
         for i in range(len(costs)):
-            heappush(prof, ( -(costs[i][1] - costs[i][0]), i))
+            heappush(prof, (costs[i][1] - costs[i][0], i))
+        
+        prof.sort(reverse = True)
         
         for i in range(len(costs)):
             if i < (len(costs)/2):
-                tot_sum += costs[heappop(prof)[1]][0]
+                tot_sum += costs[prof[i][1]][0]
             else:
-                tot_sum += costs[heappop(prof)[1]][1]
+                tot_sum += costs[prof[i][1]][1]
         return tot_sum
