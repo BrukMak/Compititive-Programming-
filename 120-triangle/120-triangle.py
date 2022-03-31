a62a804cbf6301022f292ac1a0d415a55e866f3e
BrukMak <60716114+BrukMak@users.noreply.github.com>
2022-03-31 19:34:49 +0300

    Time: 97 ms (50.40%), Space: 14.9 MB (93.29%) - LeetHub

--- 120-triangle/120-triangle.py
+++ 120-triangle/120-triangle.py
@@ -1,20 +1,32 @@
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
-        # min_path = 0
-        memo = {}
-        def dfs(level, idx):
-            # nonlocal min_path
-            if level == len(triangle) - 1:
-                return triangle[level][idx]
+        
+#         memo = {}
+#         def dfs(level, idx):
+#             # nonlocal min_path
+#             if level == len(triangle) - 1:
+#                 return triangle[level][idx]
             
-            if (level,idx) in memo: return memo[(level,idx)]
+#             if (level,idx) in memo: return memo[(level,idx)]
             
-            path1 = dfs(level + 1, idx)
-            path2 = dfs(level + 1, idx + 1)
+#             path1 = dfs(level + 1, idx)
+#             path2 = dfs(level + 1, idx + 1)
             
-            memo[(level,idx)] = min(path1, path2) + triangle[level][idx]
+#             memo[(level,idx)] = min(path1, path2) + triangle[level][idx]
             
-            return memo[(level,idx)]
+#             return memo[(level,idx)]
         
         
-        return dfs(0,0)
+#         return dfs(0,0)    
+        N = len(triangle)
+        
+        dp = triangle[-1].copy()
+        for level in range(N - 2, -1, -1):
+            for idx in range(level+1):
+                path1 = dp[idx]
+                path2 = dp[idx+1]
+                dp[idx] = min(path1, path2) + triangle[level][idx]
+            
+        return dp[0]
+                
+                
