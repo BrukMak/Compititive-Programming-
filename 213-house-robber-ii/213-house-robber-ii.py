40c7820c64e0ed7120407d965d2f8a5903f4f228
BrukMak <60716114+BrukMak@users.noreply.github.com>
2022-04-04 00:28:53 +0300

    Time: 60 ms (18.52%), Space: 13.9 MB (76.38%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.py
+++ 213-house-robber-ii/213-house-robber-ii.py
@@ -2,16 +2,16 @@ class Solution:
     def rob(self, nums: List[int]) -> int:
         if len(nums) == 1:
             return nums[0]
-        memo = {}
+        memo = [-1]*len(nums)
         def dp(idx, num):
             if idx >= len(num):
                 return 0
-            if idx in memo:
+            if memo[idx] != -1:
                 return memo[idx]
             memo[idx] = max(dp(idx+2, num) + num[idx], dp(idx + 1, num))
             return memo[idx]
         path1 = dp(1, nums)
-        memo = {}
+        memo = [-1]*len(nums)
         nums = nums[:len(nums)-1]
         path2 = dp(0, nums)
         
