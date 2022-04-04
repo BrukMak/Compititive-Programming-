136e7956e39acad00069063d072c2e0ea9de549b
BrukMak <60716114+BrukMak@users.noreply.github.com>
2022-04-04 19:25:22 +0300

    Time: 102 ms (38.18%), Space: 16.7 MB (15.65%) - LeetHub

---
+++ 740-delete-and-earn/740-delete-and-earn.py
@@ -0,0 +1,30 @@
+class Solution:
+    def deleteAndEarn(self, nums: List[int]) -> int:
+        nums.sort()
+        freq = defaultdict(int)
+        for i in nums:
+            freq[i] += 1
+        
+        set_nums = set(nums)
+        set_nums = list(set_nums)
+        ans = [0] * len(set_nums)
+        
+        ans[-1] = set_nums[-1] * freq[set_nums[-1]] 
+        for i in range(len(ans)-2, -1, -1):
+            if set_nums[i] + 1 < set_nums[i+1]:
+                ans[i] = ans[i + 1] + set_nums[i] * freq[set_nums[i]] 
+            else:
+                if i + 2 < len(ans):
+                    temp = ans[i+2] + set_nums[i] * freq[set_nums[i]]
+                    ans[i] = max(temp, ans[i+1])
+                else:
+                    temp = set_nums[i] * freq[set_nums[i]]
+                    if i+1 < len(ans):
+                        ans[i] = max(temp, ans[i+1])
+                    else:
+                        ans[i] = temp
+           
+        return ans[0]
+                
+            
+                
