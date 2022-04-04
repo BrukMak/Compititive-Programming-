5926cf6178658b1203bbccbfb661a5ff9492db37
BrukMak <60716114+BrukMak@users.noreply.github.com>
2022-04-04 11:21:16 +0300

    Time: 156 ms (87.81%), Space: 15.2 MB (49.07%) - LeetHub

--- 724-find-pivot-index/724-find-pivot-index.py
+++ 724-find-pivot-index/724-find-pivot-index.py
@@ -1,8 +1,9 @@
 class Solution:
     def pivotIndex(self, nums: List[int]) -> int:
-        sum_ = sum(nums)
+        sum_ = 0
         pre = 0
-        
+        for item in nums:
+            sum_ += item
         for i in range(len(nums)):
             post = sum_ - pre - nums[i]
             if pre == post:
