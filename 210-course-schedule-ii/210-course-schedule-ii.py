ffca8a3d0c6ab369e03e5be1bd039a263934197b
BrukMak <60716114+BrukMak@users.noreply.github.com>
2022-04-09 11:41:36 +0300

    Time: 92 ms (99.62%), Space: 17.8 MB (5.27%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.py
+++ 210-course-schedule-ii/210-course-schedule-ii.py
@@ -1,34 +1,38 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        graph, visited = defaultdict(list), [0] * numCourses
+        graph, visited = defaultdict(list), set() 
         ans = []
-        white = 0
-        grey = 1
-        black = 2
+        cycle = set()
+        safe = set()
+        
         for i in range(numCourses):
             graph[i] = []
         for course, pre in prerequisites:
             graph[pre].append(course)
         
         def dfs(node):
-            flag = True
-            visited[node] = grey
+            if node in cycle: return False
+            if node in safe: return True
+            
+            if node in visited:
+                cycle.add(node)
+                return False
+            
+            visited.add(node)
             
             for neighbour in graph[node]:
-                if visited[neighbour] == white:
-                    flag = dfs(neighbour) 
-                elif visited[neighbour] == grey:
+                if not dfs(neighbour):
+                    cycle.add(node)
                     return False
-                if not flag:
-                    return flag
                     
             ans.append(node)
-            visited[node] = black
+            safe.add(node)
             return True
+        
         for key in graph:
-            if visited[key] == white:
-                if not dfs(key):
-                    return []
+            if not dfs(key):
+                return []
+            
         return ans[::-1]
             
             
