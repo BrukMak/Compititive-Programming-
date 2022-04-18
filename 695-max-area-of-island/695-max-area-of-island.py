29ff428974fe4b02d7409f17a9ff583fbbab0c80
BrukMak <60716114+BrukMak@users.noreply.github.com>
2022-04-19 01:55:07 +0300

    Time: 226 ms (30.04%), Space: 15.2 MB (72.85%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.py
+++ 695-max-area-of-island/695-max-area-of-island.py
@@ -1,29 +1,49 @@
 class Solution:
-    def dfs(self, row, col, grid):
-        
-        if row < 0 or row >= len(grid) or col < 0 or col >= len(grid[0]) or grid[row][col] == 0:
-            return
-        
-        self.area += 1
-        grid[row][col] = 0
-
-        self.dfs(row + 1, col, grid)
-        self.dfs(row - 1, col, grid)
-        self.dfs(row, col + 1, grid)
-        self.dfs(row, col - 1, grid)  
+    def maxAreaOfIsland(self, grid):
+        rows = len(grid)
+        cols = len(grid[0])
+        parent = {}
+        size = {}
+        self.max_ = 0
+        def make_set(t):
+            parent[t] = t
+            size[t] = 1
+        def find(t):
              
-                
-            
-    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+            if parent[t] == t:
+                return t
+            parent[t] = find(parent[t])
+            return parent[t]
+        def union(t1, t2):
+            a = find(t1)
+            b = find(t2)
+            if a != b:
+                if size[a] < size[b]:
+                    a, b = b, a
+                parent[b] = a
+                size[a] += size[b]
+                self.max_ = max(self.max_, size[a])
         
-        ans = 0
         for row in range(len(grid)):
             for col in range(len(grid[0])):
                 if grid[row][col] == 1:
-                    self.area = 0
-                    self.dfs(row,col,grid)
-                    print(self.area)
-                    ans = max(ans, self.area)
-                                        
-        return ans
+                    tup = (row, col)
+                    make_set(tup)
+                    self.max_ = max(size[tup], 1)
+        
+        Dir = [(0, 1),(1, 0), (0, -1), (-1, 0)]
+        for row in range(len(grid)):
+            for col in range(len(grid[0])):
+                if grid[row][col] == 1:
+                    tup = (row, col)
                     
+                    for d in Dir:
+                        if 0 <=row + d[0] < rows and 0 <= col + d[1] < cols and grid[row + d[0]][col + d[1]] == 1:
+                              union(tup, (row + d[0], col + d[1]))
+        
+        return self.max_
+                    
+        
+        
+        
+    
