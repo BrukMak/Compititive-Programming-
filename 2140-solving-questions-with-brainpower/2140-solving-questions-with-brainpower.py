f10b99fa53d8d87faa6f5b4de0b48a2098d0c364
BrukMak <60716114+BrukMak@users.noreply.github.com>
2022-04-19 18:38:14 +0300

    Time: 2942 ms (16.56%), Space: 152 MB (35.12%) - LeetHub

---
+++ 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.py
@@ -0,0 +1,11 @@
+class Solution:
+    def mostPoints(self, questions: List[List[int]]) -> int:
+        memo = {}
+        def dp(idx):
+            if idx >= len(questions):
+                return 0
+            if idx not in memo:
+                memo[idx] = max((questions[idx][0] + dp(questions[idx][1] + idx + 1)) , dp(idx + 1))
+                
+            return memo[idx]
+        return dp(0)
