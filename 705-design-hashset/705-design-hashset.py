78ff0515d3e0e2683383ff4477c4b8b1b441da9e
BrukMak <60716114+BrukMak@users.noreply.github.com>
2022-04-21 22:41:19 +0300

    Time: 149 ms (96.36%), Space: 18.8 MB (68.57%) - LeetHub

--- 705-design-hashset/705-design-hashset.py
+++ 705-design-hashset/705-design-hashset.py
@@ -1,20 +1,19 @@
 class MyHashSet:
 
     def __init__(self):
-        self.set_ = []
+        self.set_ = {}
 
     def add(self, key: int) -> None:
         if key not in self.set_:
-            self.set_.append(key)
+            self.set_[key] = 0
 
     def remove(self, key: int) -> None:
         if key in self.set_:
-            self.set_.remove(key)
+            del self.set_[key]
         
     def contains(self, key: int) -> bool:
-        if key in self.set_:
-            return True
-        return False
+        
+        return key in self.set_
 
 
 # Your MyHashSet object will be instantiated and called as such:
