5798b07e35553667c8ab02bf9456fe63d4835735
BrukMak <60716114+BrukMak@users.noreply.github.com>
2022-04-22 22:27:45 +0300

    Time: 145 ms (38.06%), Space: 16.1 MB (9.68%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,8 +1,21 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        di = defaultdict(list)
+        for idx, val in enumerate(nums):
+            di[val].append(idx)
+        nums.sort()
         
-        
-        for idx,val in enumerate(nums):
-            for j in range(idx+1, len(nums)):
-                if val + nums[j] == target:
-                    return [idx, j]
+        for idx, val in enumerate(nums):
+            l, r = idx+1, len(nums)-1
+            while l <= r:
+                mid = l + (r-l) // 2
+                
+                if val + nums[mid] == target:
+                    if val == nums[mid]: 
+                        return di[val]
+                    return [di[val][0], di[nums[mid]][0]]
+                    
+                elif val + nums[mid] > target:
+                    r = mid - 1
+                else: l = mid + 1
+                
