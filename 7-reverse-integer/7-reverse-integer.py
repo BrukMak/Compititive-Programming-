362644bd5decfbf6861c341fcfaefee8dfd8d0bb
BrukMak <60716114+BrukMak@users.noreply.github.com>
2022-04-26 08:43:40 +0300

    Time: 56 ms (24.86%), Space: 13.9 MB (17.04%) - LeetHub

---
+++ 7-reverse-integer/7-reverse-integer.py
@@ -0,0 +1,16 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        if x >= 0:
+            x = str(x)
+            x = x[::-1]
+            x = int(x)
+        else:
+            x = str(x)
+            x = x[1:]
+            x = x[::-1]
+            x = int(x) * -1
+        if abs(x) > 2**31:
+            return 0
+        return x
+            
+        
