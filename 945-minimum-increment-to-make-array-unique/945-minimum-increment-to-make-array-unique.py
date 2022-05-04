baed87e79d1f79f46bc3f583d5fc15b44906a363
BrukMak <60716114+BrukMak@users.noreply.github.com>
2022-05-04 11:15:44 +0300

    Time: 1380 ms (27.12%), Space: 28.1 MB (76.71%) - LeetHub

--- 945-minimum-increment-to-make-array-unique/945-minimum-increment-to-make-array-unique.py
+++ 945-minimum-increment-to-make-array-unique/945-minimum-increment-to-make-array-unique.py
@@ -5,8 +5,8 @@ class Solution:
         for i in range(1, len(nums)):
             if nums[i-1] >= nums[i]:
                 dif = (nums[i-1] - nums[i] + 1)
-                move += dif
-                nums[i] += dif
+                move += (nums[i-1] - nums[i] + 1)
+                nums[i] = nums[i-1] + 1
             
         return move
             
