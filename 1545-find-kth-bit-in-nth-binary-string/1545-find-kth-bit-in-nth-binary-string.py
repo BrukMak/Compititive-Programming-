293c53a9233bd15597143c4a7f61b027ca7b143c
BrukMak <60716114+BrukMak@users.noreply.github.com>
2022-05-05 02:58:41 +0300

    Update 1545-find-kth-bit-in-nth-binary-string.py

--- 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.py
+++ 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.py
@@ -1,3 +1,4 @@
+#Recursive
 class Solution:
     def findKthBit(self, n: int, k: int) -> str:
         def invert(st):
@@ -13,3 +14,17 @@ class Solution:
             return s + "1" + invert(s)[::-1]
          
         return binaryStr(n)[k-1]
+    
+#Itrative
+class Solution:
+    def findKthBit(self, n: int, k: int) -> str:
+        s = "0"
+        for i in range(n-1):
+            ptr = 0
+            inverse =  ""
+            while ptr < len(s):
+                if int(s[ptr]): inverse += "0"
+                else: inverse += "1"
+                ptr += 1
+            s += "1" + inverse[::-1]
+        return s[k-1]
