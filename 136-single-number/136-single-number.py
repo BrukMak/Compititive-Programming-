7e1753be31d72b41433edb257ea95f387970f5a0
BrukMak <60716114+BrukMak@users.noreply.github.com>
2022-05-12 08:59:21 +0300

    Time: 196 ms (44.67%), Space: 16.6 MB (85.20%) - LeetHub

--- 136-single-number/136-single-number.py
+++ 136-single-number/136-single-number.py
@@ -1,6 +1,8 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        ans = 0
-        for num in nums:
-            ans ^= num
-        return ans
+        di = Counter(nums)
+        
+        for key,val in di.items():
+            if val != 2:
+                return key
+        
