03780c51fbcbb9058d14f74b57f92336389d2795
BrukMak <60716114+BrukMak@users.noreply.github.com>
2022-05-13 09:09:27 +0300

    Time: 51 ms (39.84%), Space: 14.1 MB (81.39%) - LeetHub

--- 78-subsets/78-subsets.py
+++ 78-subsets/78-subsets.py
@@ -1,15 +1,13 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         res = []
-        
-        def rec(idx, cur_set):
-            if idx >= len(nums):
-                res.append(cur_set)
-                return 
-            
-            rec(idx + 1, cur_set + [nums[idx]])
-            rec(idx+1, cur_set)
-            
-        rec(0, [])
-        return res 
+        for i in range(2**len(nums)):
+            cur_set = []
+            mask = list(bin(i)[2:].strip())
+            dif = len(nums) - len(mask)
+            for j in range(len(mask)-1, -1, -1):
+                if mask[j] == '1':
+                    cur_set.append(nums[j+dif])
+            res.append(cur_set)
+        return res
             
