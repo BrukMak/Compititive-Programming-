5f553162cba680382fd079d1e2e17221e58457b1
BrukMak <60716114+BrukMak@users.noreply.github.com>
2022-07-07 23:42:26 +0300

    Time: 70 ms (43.59%), Space: 16.9 MB (5.33%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.py
+++ 97-interleaving-string/97-interleaving-string.py
@@ -2,20 +2,24 @@ class Solution:
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
         if len(s3) != (len(s1) + len(s2)):
             return False
-        ls1 = len(s1)
-        ls2 = len(s2)
+        
         @lru_cache(maxsize = None)
         
-        def dp(i , j):
-            if i == len(s1) and j == len(s2):
+        def dp(s1, s2, s3):
+            if s1 == "" and s2 == "" and s3 == "":
                 return True
             
-            ch1, ch2 = False, False
-            
-            if i < ls1 and s1[i] == s3[i+j]:
-                ch1 = dp(i+1, j)
-            if j < ls2 and s2[j] == s3[i+j]:
-                ch2 = dp(i, j+1)
-            return ch1 or ch2
-        return dp(0, 0)
+            if len(s1) > 0 and len(s2) > 0 and s1[0] == s3[0] and s2[0] == s3[0]:
+                
+                return dp(s1[1:], s2, s3[1:]) or dp(s1, s2[1:], s3[1:])
+                
+            elif len(s1) > 0 and s1[0] == s3[0]:
+                
+                return dp(s1[1:], s2, s3[1:])
+            elif len(s2) > 0 and s2[0] == s3[0]:
+                
+                return dp(s1, s2[1:], s3[1:])
+            else:
+                return False
+        return dp(s1, s2, s3)
                 
