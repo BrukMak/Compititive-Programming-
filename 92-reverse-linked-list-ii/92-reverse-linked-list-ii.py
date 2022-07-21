4fa7577d225133f712b0fc544ba03005c61500a6
BrukMak <60716114+BrukMak@users.noreply.github.com>
2022-07-21 16:36:01 +0300

    Time: 48 ms (52.36%), Space: 14.2 MB (9.58%) - LeetHub

--- 92-reverse-linked-list-ii/92-reverse-linked-list-ii.py
+++ 92-reverse-linked-list-ii/92-reverse-linked-list-ii.py
@@ -5,19 +5,44 @@
 #         self.next = next
 class Solution:
     def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
-        lst = []
-        cur = head
-        while cur:
-            lst.append(cur.val)
-            cur = cur.next
-        rev = lst[left-1:right][::-1]
-        for i in range(left-1, right):
-            lst[i] = rev[i-(left-1)]
+        cur, cur2 = head, head
+        if left == right:
+            return head
+        if left > 1:
         
-        cur = head
-        for i in lst:
-            cur.val = i
-            cur = cur.next
-        return head
+            while left > 1:
+                prev = cur
+                cur = cur.next
+                left -= 1
+        else: 
+            prev = None
         
+        while right > 1:
+            cur2 = cur2.next
+            right -= 1
+        last = cur2.next
+        def rev(node):
+            
+            if node == cur2:
+                return node
+            newHead = rev(node.next)
+            node.next.next = node
+            node.next = None
+            return newHead
+        nxt = rev(cur)
+        
+        if nxt == prev: return head
+        if prev: 
+            prev.next = nxt
+            cur = head
+            while cur.next:
+                cur = cur.next
+            cur.next = last
+            return head
+        else: 
+            cur = nxt
+            while cur.next:
+                cur = cur.next
+            cur.next = last
+            return nxt
             
