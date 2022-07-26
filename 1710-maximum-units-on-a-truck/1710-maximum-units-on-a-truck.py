ecf37beaa5eabe62d9fa3b4a3c74fe40796479f8
BrukMak <60716114+BrukMak@users.noreply.github.com>
2022-07-26 23:21:27 +0300

    Time: 371 ms (12.27%), Space: 14.4 MB (84.31%) - LeetHub

--- 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.py
+++ 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.py
@@ -4,9 +4,7 @@ class Solution:
         boxTypes.sort(key = lambda x: x[1], reverse = True)
         res, ptr = 0, 0
         while ptr < len(boxTypes) and truckSize:
-            while ptr < len(boxTypes) and truckSize and boxTypes[ptr][0]:
-                res += boxTypes[ptr][1]
-                boxTypes[ptr][0] -= 1
-                truckSize -= 1
-            ptr += 1
+                res += boxTypes[ptr][1] * min(truckSize, boxTypes[ptr][0])
+                truckSize -= min(truckSize, boxTypes[ptr][0])
+                ptr += 1
         return res
