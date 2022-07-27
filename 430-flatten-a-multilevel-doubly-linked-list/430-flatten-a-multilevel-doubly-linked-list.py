7181923fd81305b740b72a2d437b26b13daad6ae
BrukMak <60716114+BrukMak@users.noreply.github.com>
2022-07-27 23:44:58 +0300

    Time: 53 ms (58.73%), Space: 15.1 MB (24.50%) - LeetHub

--- 430-flatten-a-multilevel-doubly-linked-list/430-flatten-a-multilevel-doubly-linked-list.py
+++ 430-flatten-a-multilevel-doubly-linked-list/430-flatten-a-multilevel-doubly-linked-list.py
@@ -15,20 +15,23 @@ class Solution:
             if not node:
                 return 
             if node.child == node.next:
-                return (node, node)
+                return node
             
             
             ch = dfs(node.child)
             nxt = dfs(node.next)
             node.child = None
             if ch:
-                node.next = ch[0]
-                ch[0].prev = node
-                
+                node.next = ch
+                ch.prev = node
             if ch and nxt:
-                ch[1].next = nxt[0]
-                nxt[0].prev = ch[1]
-            return (node, nxt[1]) if nxt else (node, ch[1])
+                cur = ch
+                while cur.next:
+                    cur = cur.next
+                cur.next = nxt
+                nxt.prev = cur
+                
+            return node 
             
             
         dfs(head)
