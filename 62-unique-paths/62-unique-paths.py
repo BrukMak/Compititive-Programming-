38b924b2c846335b8d1362e5da198103fccb2047
BrukMak <60716114+BrukMak@users.noreply.github.com>
2022-08-01 23:20:11 +0300

    Time: 44 ms (65.74%), Space: 14.2 MB (10.96%) - LeetHub

--- 62-unique-paths/62-unique-paths.py
+++ 62-unique-paths/62-unique-paths.py
@@ -1,10 +1,7 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        memo = {}
-        # @lru_cache(None)
+        @lru_cache(None)
         def dfs(row, col):
-            if (row, col) in memo:
-                return memo[(row, col)]
             if row >= m or col >= n:
                 return 0
             if row == m-1 and col == n-1:
@@ -12,7 +9,6 @@ class Solution:
             
             down = dfs(row + 1, col)
             right = dfs(row, col + 1)
-            memo[(row,col)] = down + right
-            return memo[(row,col)]
+            return down + right
         return dfs(0,0)
             
