ddd354bfa69caed4011c018de1165576531c97b5
BrukMak <60716114+BrukMak@users.noreply.github.com>
2022-08-08 17:16:05 +0300

    Time: 731 ms (33.94%), Space: 125.6 MB (8.70%) - LeetHub

--- 877-stone-game/877-stone-game.py
+++ 877-stone-game/877-stone-game.py
@@ -1,3 +1,13 @@
 class Solution:
     def stoneGame(self, piles: List[int]) -> bool:
-        return True
+        memo = {}
+        def dp(st, e):
+            if (st, e) in memo:
+                return memo[(st, e)]
+            if st >= e:
+                return 0
+            
+            memo[(st, e)] = max(piles[st] + dp(st+1, e) , piles[e] + dp(st, e -1))
+            return memo[(st, e)]
+        return  dp(0, len(piles)-1) > sum(piles) // 2
+            
