2bedafc0a27a66154238373bbd3972ad1db9722d
BrukMak <60716114+BrukMak@users.noreply.github.com>
2022-08-24 15:32:49 +0300

    Time: 555 ms (41.16%), Space: 35.4 MB (45.16%) - LeetHub

--- 494-target-sum/494-target-sum.py
+++ 494-target-sum/494-target-sum.py
@@ -1,10 +1,13 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
-        @lru_cache(None)
+        memo = {}
         def ts(idx, target):
+            if (idx, target) in memo:
+                return memo[(idx, target)]
             if idx >= len(nums):
                 return target == 0
             add = ts(idx + 1, target+nums[idx])
             sub = ts(idx+1, target-nums[idx])
-            return add + sub
+            memo[(idx, target)] = add + sub
+            return memo[(idx, target)]
         return ts(0,target)
