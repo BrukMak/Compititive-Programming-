96033df5df66691f7d9a0fec9b2a4d4c83cf53f0
BrukMak <60716114+BrukMak@users.noreply.github.com>
2022-08-29 21:44:49 +0300

    Time: 69 ms (32.77%), Space: 13.9 MB (60.16%) - LeetHub

--- 869-reordered-power-of-2/869-reordered-power-of-2.py
+++ 869-reordered-power-of-2/869-reordered-power-of-2.py
@@ -1,16 +1,6 @@
 class Solution:
     def reorderedPowerOf2(self, n: int) -> bool:
-        digComb = defaultdict(list)
+        given = sorted(str(n))
         for i in range(30):
-            cur = list(str(2 ** i))
-            digComb[len(cur)].append(cur)
-        nums = list(str(n))
-        size = len(nums)
-        for lst in digComb[size]:
-            for i in range(size):
-                if nums[i] in lst:
-                    idx = lst.index(nums[i])
-                    lst[idx] = '$'
-                    if i == size-1: return True
-                else: break
+            if given == sorted(str(2**i)): return True
         return False
