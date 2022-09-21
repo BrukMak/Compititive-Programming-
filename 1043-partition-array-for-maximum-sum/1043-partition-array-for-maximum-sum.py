c8c1ae924158207b44394931669f6fc3accb3dbd
BrukMak <60716114+BrukMak@users.noreply.github.com>
2022-09-21 15:12:36 +0300

    Time: 748 ms (74.76%), Space: 13.8 MB (94.10%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.py
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.py
@@ -1,15 +1,31 @@
 class Solution:
     def maxSumAfterPartitioning(self, arr: List[int], k: int) -> int:
-        @lru_cache(None)
-        def dfs(nums):
-            if nums == []: return 0
+#         @cache
+#         def dfs(idx):
+#             if idx == len(arr): return 0
             
+#             res = 0
+#             val = 0
+#             for i in range(idx, min(k+idx,len(arr))):
+#                 val = max(val, arr[i])
+#                 cur  = dfs(i+1) + val*(i+1- idx)
+#                 if cur > res:
+#                     res = cur
+#             return res
+#         return dfs(0)
+        dp = [0] * (len(arr) + 1)
+        
+        for idx in range(len(arr)-1, -1, -1):
+            val = 0
             res = 0
-            for i in range(min(k,len(nums))):
-                val = max(nums[:i+1]) * (i+1)
-                cur  = dfs(nums[i+1:]) + val
-                if cur > res:
-                    res = cur
-            return res
-        return dfs(tuple(arr))
+            for i in range(idx, min(k+idx,len(arr))):
+                val = max(val, arr[i])
+                cur = val * (i + 1 - idx) + dp[i+1]
+                res = max(res, cur)
+            dp[idx] = res
+            
+        return dp[0]
+                
+                
+                
     
