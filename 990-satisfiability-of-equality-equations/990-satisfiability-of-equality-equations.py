566e7a164ce6017f3322d4a14f45a19e9660d36a
BrukMak <60716114+BrukMak@users.noreply.github.com>
2022-09-26 15:49:32 +0300

    Time: 35 ms (100.00%), Space: 14.2 MB (35.38%) - LeetHub

--- 990-satisfiability-of-equality-equations/990-satisfiability-of-equality-equations.py
+++ 990-satisfiability-of-equality-equations/990-satisfiability-of-equality-equations.py
@@ -1,11 +1,11 @@
 class Solution:
     def equationsPossible(self, equations: List[str]) -> bool:
         parent = [x for x in range(26)]
-        def union(x, y):
-            a = find(x)
-            b = find(y)
-            if a != b:
-                parent[b] = a
+        # def union(x, y):
+        #     a = find(x)
+        #     b = find(y)
+        #     if find(x) != find:
+        #         parent[b] = a
         def find(x):
             if parent[x] == x:
                 return x
@@ -14,12 +14,15 @@ class Solution:
         for eq in equations:
             n1, sign, n2 = eq[0], eq[1:3], eq[3]
             if sign == "==":
-                union(ord(n1)-97, ord(n2)-97)
+                a = find(ord(n1)-97)
+                b = find(ord(n2)-97)
+                if a != b:
+                    parent[b] = a
 
         for eq in equations:
             n1, sign, n2 = eq[0], eq[1:3], eq[3]
             if sign == "!=":
                 if find(ord(n1)-97) == find(ord(n2)-97): return False
-        
         return True
+        
                     
