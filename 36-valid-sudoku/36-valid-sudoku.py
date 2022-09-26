278ac1bc567f3a98e106d667122e98888f528109
BrukMak <60716114+BrukMak@users.noreply.github.com>
2022-09-26 21:45:12 +0300

    Time: 227 ms (17.15%), Space: 13.8 MB (99.00%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.py
+++ 36-valid-sudoku/36-valid-sudoku.py
@@ -1,26 +1,18 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
+        row = defaultdict(set)
+        col = defaultdict(set)
+        sub_box = defaultdict(set)
         
-        for i in range(9):
-            cntR = defaultdict(int)
-            cntC = defaultdict(int)
-            for j in range(9):
-                if board[i][j] != "." and board[i][j] not in cntR:
-                    cntR[board[i][j]] += 1
-                elif board[i][j] in cntR:
-                    return False
+        for r in range(len(board)):
+            for c in range(len(board)):
+                if board[r][c] in row[r]: return False
+                if board[r][c] in col[c]: return False
+                if board[r][c] in sub_box[(r // 3, c // 3)]: return False
                 
-                if board[j][i] != "." and board[j][i] not in cntC:
-                    cntC[board[j][i]] += 1
-                elif board[j][i] in cntC:
-                    return False
+                if board[r][c] != ".":
+                    row[r].add(board[r][c])
+                    col[c].add(board[r][c])
+                    sub_box[(r // 3, c // 3)].add(board[r][c])
                 
-        for i in range(3):
-            for j in range(3):
-                cnt = defaultdict(int)
-                for row in range(i*3, (i*3 + 3)):
-                    for col in range(j*3, (j*3 + 3)):
-                        if  board[row][col] in cnt:
-                            return False
-                        if board[row][col] != ".": cnt[board[row][col]] += 1
         return True
