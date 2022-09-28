c21c50840afc1513b1593eb8cb31bba1d5b50870
BrukMak <60716114+BrukMak@users.noreply.github.com>
2022-09-28 14:51:41 +0300

    Time: 3593 ms (20.51%), Space: 25.1 MB (23.18%) - LeetHub

--- 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.py
+++ 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.py
@@ -1,18 +1,22 @@
 class NumMatrix:
 
     def __init__(self, matrix: List[List[int]]):
-        self.matrix = matrix
-        self.rows = len(matrix)
-        self.cols =len(matrix[0])
-        self.pre = [[0] * (self.cols+1) for _ in range(self.rows + 1)]
-        # self.pre[1][1] = matrix[0][0]
-        for i in range(1, self.rows + 1):
-            for j in range(1, self.cols + 1):
-                self.pre[i][j] = self.pre[i-1][j] + self.pre[i][j-1] - self.pre[i-1][j-1] + matrix[i-1][j-1]
-        print(self.pre)
+        
+        rows, cols = len(matrix), len(matrix[0])
+
+        self.prefixSum = [[0] * (cols+1) for _ in range(rows + 1)]
+        
+        for i in range(1, rows + 1):
+            for j in range(1, cols + 1):
+                prevRowPrefix = self.prefixSum[i-1][j]
+                prevColPrefix =  self.prefixSum[i][j-1]
+                cornerElement = self.prefixSum[i-1][j-1]
+                mySelf = matrix[i-1][j-1]
+                self.prefixSum[i][j] = prevRowPrefix + prevColPrefix - cornerElement + mySelf
+                
     def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
         
-        ans = self.pre[row2+1][col2+1] - self.pre[row2+1][col1] - self.pre[row1][col2+1] + self.pre[row1][col1]
+        ans = self.prefixSum[row2+1][col2+1] - self.prefixSum[row2+1][col1] - self.prefixSum[row1][col2+1] + self.prefixSum[row1][col1]
         
         return ans
 # Your NumMatrix object will be instantiated and called as such:
