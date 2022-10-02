5262d17fbdd3148fbe63e4c132514503dae8103e
BrukMak <60716114+BrukMak@users.noreply.github.com>
2022-10-02 22:32:18 +0300

    Time: 364 ms (83.68%), Space: 20.5 MB (20.73%) - LeetHub

---
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.py
@@ -0,0 +1,16 @@
+class Solution:
+    def numRollsToTarget(self, n: int, k: int, target: int) -> int:
+        @cache
+        def numRollsToTargetHelper(cur_die, cur_sum):
+            if cur_die == n:
+                return 1 if cur_sum == target else 0
+            # if (cur_die, cur_sum) in memcache:
+            #     return memcache[(cur_die, cur_sum)]
+
+            res = 0
+            for i in range(1, k + 1):
+                res += numRollsToTargetHelper(cur_die + 1, cur_sum + i)
+
+            return res
+
+        return numRollsToTargetHelper(0, 0) % (10**9 + 7)
