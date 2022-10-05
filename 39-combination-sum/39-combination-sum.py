61ff22a7ce88fb9bd63b5989cd0a744e48e165d1
BrukMak <60716114+BrukMak@users.noreply.github.com>
2022-10-05 21:42:03 +0300

    Time: 176 ms (28.61%), Space: 19.5 MB (5.13%) - LeetHub

--- 39-combination-sum/39-combination-sum.py
+++ 39-combination-sum/39-combination-sum.py
@@ -2,6 +2,7 @@ class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         res = []
         N = len(candidates)
+        @cache
         def allSum(cur, c_sum, idx):
             if c_sum == target:
                 res.append(list(cur))
