d4093ce99c640a3033cd1088e531446403b87841
BrukMak <60716114+BrukMak@users.noreply.github.com>
2022-10-08 22:43:42 +0300

    Time: 92 ms (83.74%), Space: 14 MB (82.76%) - LeetHub

--- 39-combination-sum/39-combination-sum.py
+++ 39-combination-sum/39-combination-sum.py
@@ -2,13 +2,13 @@ class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         res = []
         def uniqueComb(idx, curArr, tar):
-            if tar < 0: return
             if idx >= len(candidates):
                 if tar == 0:
                     # print(curArr)
                     res.append(curArr.copy())
                 return
-            uniqueComb(idx, curArr + [candidates[idx]], tar - candidates[idx])
+            if tar - candidates[idx] >= 0:
+                uniqueComb(idx, curArr + [candidates[idx]], tar - candidates[idx])
             uniqueComb(idx + 1, curArr, tar)
                         
         uniqueComb(0, [], target)
