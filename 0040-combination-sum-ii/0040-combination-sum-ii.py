8952163415e3f8ff17082d07987cc723bba43952
BrukMak <60716114+BrukMak@users.noreply.github.com>
2022-10-14 08:55:36 +0300

    Time: 48 ms (98.09%), Space: 14.1 MB (23.72%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.py
+++ 0040-combination-sum-ii/0040-combination-sum-ii.py
@@ -1,27 +1,21 @@
 class Solution:
         
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
-        res = set()
+        res = []
         N = len(candidates)
         candidates.sort()
-        @cache
         def helper(idx, curArr, target):
-            if idx >= N:
-                if target == 0:
-                    ans = list(curArr).copy()
-                    
-                    res.add(tuple(ans))
+            if target == 0:
+                res.append(curArr.copy())
                 return
-            if target - candidates[idx] >= 0:
-                helper(idx + 1, curArr + (candidates[idx],), target - candidates[idx])
-            helper(idx + 1, curArr, target)
             
-        helper(0, (), target)
+            for i in range(idx, N):
+                if i != idx and candidates[i] == candidates[i-1]:
+                    continue #Skip duplicate starting point
+                if target < candidates[i]: 
+                    break # target is lower than current candidate
+                helper(i+1, curArr + [candidates[i]], target - candidates[i])
+            
+        helper(0, [], target)
         return res
     
-    """
-        concatnating to a tuple => (a,b) + (c,)
-        reurning set() => could be evaluated as list
-        set of tuple could be considered as list of list
-        
-    """
