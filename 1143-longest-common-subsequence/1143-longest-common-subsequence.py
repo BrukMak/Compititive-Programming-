19bd3ca95a6b3e20d90d16c4e0f5f590cc3eb058
BrukMak <60716114+BrukMak@users.noreply.github.com>
2022-10-21 20:34:36 +0300

    Time: 382 ms (96.92%), Space: 22.1 MB (69.00%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -1,20 +1,37 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        @cache
-        def helper(pointer1, pointer2):
+#         @cache
+#         def helper(pointer1, pointer2):
             
-            if pointer1 >= len(text1) or pointer2 >= len(text2):
-                return 0
+#             if pointer1 >= len(text1) or pointer2 >= len(text2):
+#                 return 0
             
-            answer = 0
-            if text1[pointer1] == text2[pointer2]:
-                return 1 + helper(pointer1 + 1, pointer2 + 1)
-            else:
-                choice1 = helper(pointer1 + 1, pointer2)
-                choice2 = helper(pointer1, pointer2 + 1)
+#             answer = 0
+#             if text1[pointer1] == text2[pointer2]:
+#                 return 1 + helper(pointer1 + 1, pointer2 + 1)
+#             else:
+#                 choice1 = helper(pointer1 + 1, pointer2)
+#                 choice2 = helper(pointer1, pointer2 + 1)
                 
-                answer += max(choice1, choice2)
-            return answer
+#                 answer += max(choice1, choice2)
+#             return answer
         
-        return helper(0, 0)
+#         return helper(0, 0)
+        N1 = len(text1)
+        N2 = len(text2)
+        dp = [[0] * (N2 + 1) for _ in range(N1 + 1)]
+        dp[N1][N2] = 0
+        
+        for pointer1 in range(N1-1, -1, -1):
+            for pointer2 in range(N2-1, -1, -1):
+                answer = 0
+                if text1[pointer1] == text2[pointer2]:
+                    answer = 1 + dp[pointer1 + 1][pointer2 + 1]
+                else:
+                    choice1 = dp[pointer1 + 1][pointer2]
+                    choice2 = dp[pointer1][pointer2 + 1]
+                
+                    answer = max(choice1, choice2)
+                dp[pointer1][pointer2] = answer
+        return dp[0][0]
         
