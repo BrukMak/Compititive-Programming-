8d4cf4f3c7b568441f9cf8d38e9ff702ccf32002
BrukMak <60716114+BrukMak@users.noreply.github.com>
2022-10-27 08:00:04 +0300

    Time: 375 ms (82.07%), Space: 19.2 MB (9.96%) - LeetHub

--- 1392-longest-happy-prefix/1392-longest-happy-prefix.py
+++ 1392-longest-happy-prefix/1392-longest-happy-prefix.py
@@ -1,16 +1,24 @@
 class Solution:
     def longestPrefix(self, s: str) -> str:
-        lps = [0] * len(s)
-        for i in range(1, len(s)):
-            j = lps[i-1]
-            while j and s[i] != s[j]:
-                j = lps[j-1]
-                
-            if s[i] == s[j]:
-                lps[i] = j + 1                
-
-        # print(lps)
+        lps = self.findLPS(s)
         last_value = lps[-1]
         answer = s[:last_value]
         return answer              
+        
+    def findLPS(self, pattern):
+        lps = [0] * len(pattern)
+        length = 0
+        running = 1
+        while running < len(pattern):
+            if pattern[running] == pattern[length]:
+                lps[running] = length + 1
+                running += 1
+                length += 1
+            else:
+                if length != 0:
+                    length = lps[length - 1]
+                else:
+                    running += 1
+        return lps
+        
             
