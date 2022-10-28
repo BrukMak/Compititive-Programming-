28ebce450c90b1e40916027229d16ca420d231fb
BrukMak <60716114+BrukMak@users.noreply.github.com>
2022-10-28 22:12:41 +0300

    Time: 109 ms (91.06%), Space: 17.2 MB (88.69%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -2,14 +2,10 @@ class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         group = defaultdict(list)
         for word in strs:
-            curWord = [*word]
-            curWord.sort()
-            sorted_word = "".join(curWord)
+            sorted_word = "".join(sorted(word))
             group[sorted_word].append(word)
-        answer = []
-        for val in group.values():
-            answer.append(val)
-        answer.sort()
+        
+        answer = sorted(group.values())
         return answer
             
                 
