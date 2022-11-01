d4281acb852028e648a957c4d60be158f8fe7dac
BrukMak <60716114+BrukMak@users.noreply.github.com>
2022-11-01 21:59:11 +0300

    Time: 1794 ms (5.00%), Space: 87.3 MB (5.21%) - LeetHub

--- 0376-wiggle-subsequence/0376-wiggle-subsequence.py
+++ 0376-wiggle-subsequence/0376-wiggle-subsequence.py
@@ -1,25 +1,23 @@
 class Solution:
     def wiggleMaxLength(self, nums: List[int]) -> int:
         
-        
-        @cache
-        def helper(prev, index, flag):
-            # state = (prev, index, flag)
-            # if state in memo:
-            #     return memo[state]
-
-            if index >= len(nums):
-                return 0
-            take = 0
-            skip = 0
-            if (flag and nums[index] > nums[prev]) or (not flag and nums[index] < nums[prev]):
-                take = 1 + helper(index, index + 1, not flag)
-            skip = helper(prev, index + 1, flag)
-            # memo[state] = max(take, skip)
-            return max(take, skip)
-        
-        positive = helper(0, 1, True)
-        negative = helper(0, 1, False)
+        positive = self.helper(0, 1, True, nums, {})
+        negative = self.helper(0, 1, False, nums, {})
         
         return max(positive, negative) + 1
+    
+    def helper(self, prev, index, flag, nums, memo):
+        state = (prev, index, flag)
+        if state in memo:
+            return memo[state]
+        
+        if index >= len(nums):
+            return 0
+        take = 0
+        skip = 0
+        if (flag and nums[index] > nums[prev]) or (not flag and nums[index] < nums[prev]):
+            take = 1 + self.helper(index, index + 1, not flag, nums, memo)
+        skip = self.helper(prev, index + 1, flag, nums, memo)
+        memo[state] = max(take, skip)
+        return max(take, skip)
             
