0e1c703cb87525d35cae7d8629c8eb08bd45a697
BrukMak <60716114+BrukMak@users.noreply.github.com>
2022-11-02 16:18:35 +0300

    Time: 304 ms (8.57%), Space: 14 MB (16.05%) - LeetHub

--- 0980-unique-paths-iii/0980-unique-paths-iii.py
+++ 0980-unique-paths-iii/0980-unique-paths-iii.py
@@ -1,6 +1,11 @@
+"""
+Using bits to represent visited
+"""
+
 class Solution:
     answer = 0
     def uniquePathsIII(self, grid: List[List[int]]) -> int:
+        
         rows = len(grid)
         cols = len(grid[0])
         empty_count = 0
@@ -13,10 +18,12 @@ class Solution:
                 if grid[row][col] == 1:
                     start_row = row
                     start_col = col
-        visited = set()
-        visited.add((start_row, start_col))
+        visited = 0
+        visited |= 1 << self.position(start_row, start_col, grid)
         self.backTrack(start_row, start_col, 0, empty_count, grid, directions, visited)
         return self.answer
+    def position(self, row, col, grid):
+        return (len(grid[0]) * row) + col
     
     def inBound(self, row, col, grid):
         return 0 <= row < len(grid) and 0 <= col < len(grid[0])
@@ -25,19 +32,15 @@ class Solution:
     def backTrack(self, row, col, path, empty_count, grid, directions, visited):
 
         if path - 1 == empty_count and grid[row][col] == 2:
-            # print('here')
             self.answer += 1
             return
-        # if (row, col) in visited:
-        #     return
         
         for x, y in directions:
             new_row, new_col = row + x, col + y
-            
-            if self.inBound(new_row, new_col, grid) and grid[new_row][new_col] != -1 and (new_row, new_col) not in visited:
-                visited.add((new_row, new_col))
-                
-                self.backTrack(new_row, new_col, path + 1, empty_count, grid, directions, visited)
-                # path -= 1
-                visited.remove((new_row, new_col))
+            if self.inBound(new_row, new_col, grid):
+                toCheck = 1 << (self.position(new_row, new_col, grid))
+                if grid[new_row][new_col] != -1 and (new_row, new_col) and not (visited & toCheck):
+                    visited |= 1 << self.position(new_row, new_col, grid)
+                    self.backTrack(new_row, new_col, path + 1, empty_count, grid, directions, visited)
+                    visited &= ~(1 << self.position(new_row, new_col, grid))
         
