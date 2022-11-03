a6ef83b462f14ae0fe796ffa4a5a158a07536d10
BrukMak <60716114+BrukMak@users.noreply.github.com>
2022-11-03 23:56:48 +0300

    Time: 3996 ms (11.36%), Space: 38.4 MB (54.66%) - LeetHub

--- 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.py
+++ 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.py
@@ -1,23 +1,20 @@
 class Solution:
     def longestPalindrome(self, words: List[str]) -> int:
         dup_odd = 0
-        dup_even = 0
-        diff = 0
+        ans = 0
         count = Counter(words)
         
         for word in words:
             rev_word = word[::-1]
             if word == rev_word:
                 if count[word] % 2:
-                    dup_odd = max(dup_odd, count[word] * 2)
-                    dup_even += ((count[word] - 1) * 2)
-                else:
-                    dup_even += (count[word]* 2)
+                    dup_odd = 1
+                ans += (count[word] // 2 * 2)
             else:
                 if rev_word in count:
-                    diff += min(count[word], count[rev_word]) * 4
+                    ans += min(count[word], count[rev_word]) * 2
                     
                     del count[rev_word]
             del count[word]
             
-        return diff + dup_odd + dup_even - (dup_odd - 2) if dup_odd else diff + dup_even
+        return (ans + dup_odd) * 2 
