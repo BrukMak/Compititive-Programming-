4b3a63e0a46d83db99a227212cfe23dab4cbbaa9
BrukMak <60716114+BrukMak@users.noreply.github.com>
2022-11-06 19:44:56 +0300

    Time: 69 ms (70.15%), Space: 14 MB (84.81%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,17 +1,18 @@
 class Solution:
-    def backTrack(self, nums, cur, ans,flag):
+    def backTrack(self, nums, cur, ans, flag):
         if len(nums) == len(cur):
             ans.append(cur[:])
             return
             
         for i in range(len(nums)):
-            if not flag[i]:
-                flag[i] = True
+            check_point = 1 << i
+            if not (flag & check_point):
+                flag |= check_point
+                
                 self.backTrack(nums, cur + [nums[i]], ans, flag)
-                flag[i] = False
-            # print(ans)
+                flag ^= check_point
     def permute(self, nums: List[int]) -> List[List[int]]:
-        flag = [False] * len(nums)
+        flag = 0
         ans = []
         self.backTrack(nums, [], ans, flag)
         return ans
