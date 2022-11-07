77d63931130adca7c5e88d9e8cdf2d16062cbd02
BrukMak <60716114+BrukMak@users.noreply.github.com>
2022-11-07 23:15:17 +0300

    Time: 164 ms (37.52%), Space: 14.5 MB (47.41%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.py
@@ -0,0 +1,37 @@
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        board = [["."] * n for _ in range(n)]
+        answer = []
+        self.backTrack(0, board, answer, n)
+        return answer
+        
+    def backTrack(self, col, board, answer, n):
+        if col == n:
+            current = []
+            for r in board:
+                current.append("".join(r))
+            answer.append(current)
+            return
+        for row in range(n):
+            if self.checkQueen(row, col, board, n):
+                board[row][col] = 'Q'
+                self.backTrack(col + 1, board, answer, n)
+                board[row][col] = '.'
+    def checkQueen(self, row, col, board, n):
+        for i in range(n):
+            if board[row][i] == 'Q' or board[i][col] == 'Q':
+                return False
+        left_top_row, left_top_col = row - min(row, col), col - min(row, col)
+        while left_top_row < n and left_top_col < n:
+            if board[left_top_row][left_top_col] == 'Q':
+                return False
+            left_top_row += 1
+            left_top_col += 1
+        right_top_row, right_top_col = row - (n - 1 - col), col + (n - 1 - col)
+        while right_top_row < n and right_top_col >= 0:
+            if board[right_top_row][right_top_col] == 'Q':
+                return False
+            right_top_row += 1
+            right_top_col -= 1
+        return True
+    
