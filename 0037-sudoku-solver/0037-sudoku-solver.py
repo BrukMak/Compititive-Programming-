81bcfcd399cfdf09d135d1bc495aeb0c92e5f048
BrukMak <60716114+BrukMak@users.noreply.github.com>
2022-11-10 00:53:22 +0300

    Time: 2129 ms (8.41%), Space: 13.9 MB (96.18%) - LeetHub

---
+++ 0037-sudoku-solver/0037-sudoku-solver.py
@@ -0,0 +1,49 @@
+class Solution:
+    def solveSudoku(self, board: List[List[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        ans = []
+        group = []
+        for i in range(3):
+            group.append([i for i in range(i * 3, ((i + 1) * 3))])
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] == '.':
+                    end_row = r
+                    end_col = c
+        
+        self.backTrack(0, 0 , board, group, end_row, end_col)
+        
+    def isValid(self, row, col, candidate, board, group):
+        for i in range(9):
+            if board[i][col] == str(candidate) or board[row][i] == str(candidate):
+                return False
+        
+        for arr in group:
+            if row in arr:
+                for g in group:
+                    if col in g:
+                        for r in arr:
+                            for c in g:
+                                if board[r][c] == str(candidate):
+                                    return False
+        return True
+    
+    
+    def  backTrack(self,row, col, board, group, end_row, end_col):
+        if row == end_row and col == end_col:
+            return True
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] == ".":
+                    for candidate in range(1, 10):
+                        if self.isValid(r, c, candidate, board, group):
+                            board[r][c] = str(candidate)
+                            if self.backTrack(r, c, board, group, end_row, end_col):
+                                return True
+                            else:
+                                board[r][c] = "."
+                    return False
+                            
+                    
