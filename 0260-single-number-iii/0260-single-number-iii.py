d1cb69c6c7af93d5b3fed1cfaa0960c677d4a8d0
BrukMak <60716114+BrukMak@users.noreply.github.com>
2022-11-10 22:59:46 +0300

    Time: 140 ms (35.68%), Space: 15.8 MB (58.99%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.py
+++ 0260-single-number-iii/0260-single-number-iii.py
@@ -1,17 +1,26 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> List[int]:
+        # Find the xor of the whole list
         xor = 0
         for num in nums:
             xor ^= num
+        # Find the first/any position the xor is 1
+        # B/C is the xor is 1 that means one of them is 1 the other is zero at that position
         pos = self.findPos(xor)
         half1 = 0
         half2 = 0
+        
+        # Group the whole list in to two with the req being having 1 at the pos
         for num in nums:
             if num & 1 << pos:
                 half1 ^= num
             else:
                 half2 ^= num
+                
+                
         return [half1, half2]
+    
+    
     def findPos(self, xor):
         for i in range(32):
             test = xor & 1 << i
