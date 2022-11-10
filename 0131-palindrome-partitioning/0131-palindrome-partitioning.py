d76608e9f9e6036e4e7e131386632089ce1303b6
BrukMak <60716114+BrukMak@users.noreply.github.com>
2022-11-11 01:14:12 +0300

    Time: 673 ms (95.43%), Space: 29 MB (88.27%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.py
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -1,20 +1,16 @@
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
         answer = []
-        self.backTrack("", s, [], answer)
+        self.backTrack(0, s, [], answer)
         return answer
-    def backTrack(self, cur, s, cur_ds, answer):
-        if s == "":
+    def backTrack(self, index, s, cur_ds, answer):
+        if index >= len(s):
             answer.append(cur_ds[:])
             return
-        for i in range(1, len(s)+1):
-            candidate = s[:i]
-            if candidate == candidate[::-1]:
+        for i in range(index, len(s)+1):
+            candidate = s[index:i]
+            if len(candidate) > 0 and candidate == candidate[::-1]:
                 cur_ds.append(candidate)
-                cur = candidate
-                s = s[i:]
-                self.backTrack(cur, s, cur_ds, answer)
+                self.backTrack(i, s, cur_ds, answer)
                 cur_ds.pop()
-                cur = ""
-                s = candidate + s
-        
+                
