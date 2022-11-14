82b8c9c3c4760a66b94f510f712f0b298261f828
BrukMak <60716114+BrukMak@users.noreply.github.com>
2022-11-14 18:32:51 +0300

    Time: 87 ms (84.43%), Space: 14 MB (60.23%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.py
+++ 0040-combination-sum-ii/0040-combination-sum-ii.py
@@ -1,21 +1,20 @@
 class Solution:
-        
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
         res = []
         N = len(candidates)
         candidates.sort()
         def helper(idx, curArr, target):
             if target == 0:
-                res.append(curArr.copy())
+                res.append(curArr[:])
                 return
-            
+
             for i in range(idx, N):
-                if i != idx and candidates[i] == candidates[i-1]:
-                    continue #Skip duplicate starting point
-                if target < candidates[i]: 
-                    break # target is lower than current candidate
-                helper(i+1, curArr + [candidates[i]], target - candidates[i])
+                if idx != i and i > 0 and candidates[i] == candidates[i-1]:
+                    continue
+                if candidates[i] > target:
+                    break
+                helper(i + 1, curArr + [candidates[i]], target - candidates[i])
             
-        helper(0, [], target)
+        helper(0, [],target)
         return res
     
