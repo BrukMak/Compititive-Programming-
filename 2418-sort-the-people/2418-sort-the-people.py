ccaf839898e8b317226596193273d831bb809659
BrukMak <60716114+BrukMak@users.noreply.github.com>
2023-01-17 20:08:13 +0300

    Time: 1010 ms (7.74%), Space: 21.8 MB (8.95%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.py
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -1,14 +1,15 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
         
+        max_num = max(heights)
+        count = [[] for _ in range(max_num + 1)]
+        
         for i in range(len(names)):
-            isSwaped = False
-            for j in range(1, len(names)):
-                if heights[j] > heights[j-1]:
-                    heights[j], heights[j-1] = heights[j-1], heights[j]
-                    names[j], names[j-1] = names[j-1], names[j]
-                    isSwaped = True
-            if not isSwaped:
-                break
+            count[heights[i]].append(names[i])
+        ans = []
+        for i in range(len(count)-1, -1, -1):
+            l = count[i]
+            while l:
+                ans.append(l.pop())
                 
-        return names
+        return ans
