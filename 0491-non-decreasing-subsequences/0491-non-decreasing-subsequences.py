ceb72860a541a9c2119005020c941278f5e592b9
BrukMak <60716114+BrukMak@users.noreply.github.com>
2023-01-20 10:57:55 +0300

    Time: 255 ms (56.88%), Space: 22.4 MB (40.47%) - LeetHub

---
+++ 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.py
@@ -0,0 +1,20 @@
+class Solution:
+    def findSubsequences(self, nums: List[int]) -> List[List[int]]:
+        
+        self.result = set()
+        self.allSubsequence(0, nums, [])
+        return self.result
+    def allSubsequence(self, index, nums, current):
+        if index == len(nums):
+            if len(current) > 1:
+                self.result.add(tuple(current[:]))
+            return
+        # take
+        if not current or current[-1] <= nums[index]:
+            current.append(nums[index])
+            self.allSubsequence(index + 1, nums, current)
+            current.pop()
+            
+        # skip
+        self.allSubsequence(index + 1, nums, current)
+            
