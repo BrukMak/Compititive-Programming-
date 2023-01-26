af3f4e5462bef9a32319c4b3955b2b424e595695
BrukMak <60716114+BrukMak@users.noreply.github.com>
2023-01-27 00:02:34 +0300

    Time: 7675 ms (5.00%), Space: 13.8 MB (95.01%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -1,7 +1,6 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
-        
-        for num in range(2 ** 16):
+        for num in range(x // 2 + 2):
             square = num * num
             if square > x:
                 return num - 1
