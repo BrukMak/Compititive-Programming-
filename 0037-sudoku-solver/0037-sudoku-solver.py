e53e8901adb87614f9e523b3041b0e526931b017
BrukMak <60716114+BrukMak@users.noreply.github.com>
2023-02-02 12:21:37 +0300

    Time: 1037 ms (14.43%), Space: 14 MB (69.88%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.py
+++ 0037-sudoku-solver/0037-sudoku-solver.py
@@ -4,9 +4,7 @@ class Solution:
         Do not return anything, modify board in-place instead.
         """
         ans = []
-        group = []
-        for i in range(3):
-            group.append([i for i in range(i * 3, ((i + 1) * 3))])
+    
             
         for r in range(9):
             for c in range(9):
@@ -14,37 +12,31 @@ class Solution:
                     end_row = r
                     end_col = c
         
-        self.backTrack(0, 0 , board, group, end_row, end_col)
+        self.backTrack(0, 0 , board, end_row, end_col)
         
-    def isValid(self, row, col, candidate, board, group):
+    def isValid(self, row, col, candidate, board):
         for i in range(9):
             if board[i][col] == str(candidate) or board[row][i] == str(candidate):
                 return False
-        
-        for row_arr in group:
-            if row in row_arr:
-                for col_arr in group:
-                    if col in col_arr:
-                        for r in row_arr:
-                            for c in col_arr:
-                                if board[r][c] == str(candidate):
-                                    return False
+            if board[3 * (row // 3 ) + (i // 3)][3 * (col // 3) + (i % 3)] == str(candidate):
+                return False
         return True
     
     
-    def  backTrack(self,row, col, board, group, end_row, end_col):
+    def  backTrack(self,row, col, board, end_row, end_col):
         if row == end_row and col == end_col:
             return True
         for r in range(9):
             for c in range(9):
                 if board[r][c] == ".":
-                    for candidate in range(1, 10):
-                        if self.isValid(r, c, candidate, board, group):
-                            board[r][c] = str(candidate)
-                            if self.backTrack(r, c, board, group, end_row, end_col):
+                    for num in range(1, 10):
+                        if self.isValid(r, c, num, board):
+                            board[r][c] = str(num)
+                            if self.backTrack(r, c, board, end_row, end_col):
                                 return True
                             else:
                                 board[r][c] = "."
+                        
                     return False
                             
                     
