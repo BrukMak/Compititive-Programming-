2713a379aa3fafb5500c28ea0cdfcdb8a1fbe0a0
BrukMak <60716114+BrukMak@users.noreply.github.com>
2023-02-03 21:18:49 +0300

    Time: 55 ms (85.61%), Space: 14.2 MB (17.57%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.py
@@ -0,0 +1,23 @@
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        if numRows == 1: return s
+        res = [[] for _ in range(numRows)]
+        res[0].append(s[0])
+        n, idx, ptr = len(s), 1, 0
+        while idx < n:  
+            if ptr == 0:
+                ptr += 1
+                while idx < n and ptr < numRows:
+                    res[ptr].append(s[idx])
+                    ptr += 1
+                    idx += 1
+                ptr -= 1
+            if ptr == numRows-1:
+                ptr -= 1
+                while idx < n and ptr >= 0:
+                    res[ptr].append(s[idx])
+                    idx += 1
+                    ptr -= 1
+                ptr += 1
+        return "".join(["".join(l) for l in res])
+                
