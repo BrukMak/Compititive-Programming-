9179ca48e5ebe1533cbbc01c7aca732a75882e07
BrukMak <60716114+BrukMak@users.noreply.github.com>
2023-02-15 23:40:31 +0300

    Time: 62 ms (73.39%), Space: 14.2 MB (62.38%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.py
+++ 0047-permutations-ii/0047-permutations-ii.py
@@ -1,21 +1,20 @@
 class Solution:
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
-        
+        nums.sort()
         unique_permutations = []
-        self.findPermutation(nums, Counter(nums), [], unique_permutations)
+        self.findPermutation(nums, [], unique_permutations)
         
         return unique_permutations
         
-    def findPermutation(self, nums, count, current, unique_permutations):
-        if len(current) == len(nums):
+    def findPermutation(self, nums, current, unique_permutations):
+        if not nums:
             unique_permutations.append(current[:])
             return
-        for num in count:
-            if count[num] > 0: 
-                count[num] -=1
-                current.append(num)
-                self.findPermutation(nums, count, current, unique_permutations)
-                count[num] += 1
-                current.pop()
-                
+        for i in range(len(nums)):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            current.append(nums[i])
+            self.findPermutation(nums[:i]+nums[i+1:], current, unique_permutations)
+            current.pop()
+
                 
