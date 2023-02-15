6899351481987a520de97e81912f6585bcc46623
BrukMak <60716114+BrukMak@users.noreply.github.com>
2023-02-15 23:59:37 +0300

    Time: 28 ms (95.08%), Space: 13.8 MB (76.07%) - LeetHub

---
+++ 0996-number-of-squareful-arrays/0996-number-of-squareful-arrays.py
@@ -0,0 +1,26 @@
+class Solution:
+    
+    def numSquarefulPerms(self, nums: List[int]) -> int:
+        res = []
+
+        def dfs(nums, current):
+            if not nums:
+                res.append(current)
+                return 
+
+            for i in range(len(nums)):
+                if i > 0 and nums[i] == nums[i - 1]:
+                    continue
+
+                if current and not self.is_perfect_square(current[-1] + nums[i]):
+                    continue
+
+                current.append(nums[i])
+                dfs(nums[:i] + nums[i + 1:], current)
+                current.pop()
+        nums.sort()
+        dfs(nums,[])
+        return len(res)
+
+    def is_perfect_square(self,num):
+        return pow(int(sqrt(num)),2) == num
