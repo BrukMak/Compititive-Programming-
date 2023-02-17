aeb161b9292a5bb7fa6f2d6d4cd46b75730bd533
BrukMak <60716114+BrukMak@users.noreply.github.com>
2023-02-17 21:52:38 +0300

    Time: 38 ms (58.47%), Space: 14.1 MB (97.36%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -0,0 +1,20 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        res = []
+        s1, s2 = ["("] * n , [")"] * n
+        def generator(i1, i2, cur):
+            if i1 >= n and i2 >= n:
+                return cur
+            
+            if i1 == i2:
+                rop = generator(i1+1, i2, cur +s1[i1])
+                if rop: res.append(rop)
+            else:
+                if i1 < n: 
+                    op = generator(i1+1, i2, cur+s1[i1])
+                    if op != None: res.append(op)
+                if i2 < n: 
+                    cl = generator(i1, i2+1, cur+s2[i2])
+                    if cl != None: res.append(cl)
+        generator(0, 0, "")
+        return res
