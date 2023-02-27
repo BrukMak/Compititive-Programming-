23c7fe3fa9f6e443aadff1c2e3d81abf68541b79
BrukMak <60716114+BrukMak@users.noreply.github.com>
2023-02-27 23:55:01 +0300

    Time: 1349 ms (63.37%), Space: 59 MB (5.94%) - LeetHub

--- 1665-minimum-initial-energy-to-finish-tasks/1665-minimum-initial-energy-to-finish-tasks.py
+++ 1665-minimum-initial-energy-to-finish-tasks/1665-minimum-initial-energy-to-finish-tasks.py
@@ -4,14 +4,14 @@ class Solution:
         # Sort by difference
         tasks.sort(key=lambda x: x[0] - x[1])
         
-        my_energy = tasks[0][1]
-        current_task = tasks[0][1] - tasks[0][0]
+        result = tasks[0][1]
+        curr = tasks[0][1] - tasks[0][0]
         
         for i in range(1, len(tasks)):
-            if current_task < tasks[i][1]:
-                my_energy += tasks[i][1] - current_task 
-                current_task += tasks[i][1] - current_task
-            current_task -= tasks[i][0] 
+            if curr < tasks[i][1]:
+                result += tasks[i][1] - curr 
+                curr += tasks[i][1] - curr
+            curr -= tasks[i][0] 
             
             
-        return my_energy
+        return result
