cf7a1baacd6ef5b33d79c97fce851405913cd387
BrukMak <60716114+BrukMak@users.noreply.github.com>
2023-03-07 23:16:38 +0300

    Time: 457 ms (32.61%), Space: 15.8 MB (82.96%) - LeetHub

--- 0077-combinations/0077-combinations.py
+++ 0077-combinations/0077-combinations.py
@@ -1,21 +1,22 @@
 class Solution:
     
     def combine(self, n: int, k: int) -> List[List[int]]:
-        ans = []
+        combinations = []
         nums = [i for i in range(1, n+1)]
         
         def backTracking(i, curr):
-            if i >= n:
-                if len(curr) == k:
-                    ans.append(curr[:])
-            
+            if len(curr) == k:
+                combinations.append(curr[:])
                 return
+            if i >= n:
+                return
+            
             curr.append(nums[i])
             backTracking(i+1, curr)
             curr.pop()
             backTracking(i+1, curr)
             
         backTracking(0, [])
-        return ans
+        return combinations
                 
         
