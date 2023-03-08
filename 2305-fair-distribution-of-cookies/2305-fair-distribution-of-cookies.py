b77ccb0d4b2ea8f816389cbb2559304be7d412af
BrukMak <60716114+BrukMak@users.noreply.github.com>
2023-03-09 00:49:03 +0300

    Time: 2762 ms (23.53%), Space: 13.9 MB (69.49%) - LeetHub

---
+++ 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.py
@@ -0,0 +1,21 @@
+class Solution:
+    def distributeCookies(self, cookies: List[int], k: int) -> int:
+        
+        children = [0] * k
+        result = float('inf')
+        def distribute(index):
+            nonlocal result
+            if index == len(cookies):
+                result = min(result, max(children))
+                return 
+            if max(children) >= result:
+                return 
+            for i in range(k):
+                if result > children[i] + cookies[index]:
+                    children[i] += cookies[index]
+                    distribute(index + 1)
+                    children[i] -= cookies[index]
+                
+        distribute(0)
+        
+        return result
