f4fe290184198cac5340a9a3dfa43f3494710e54
BrukMak <60716114+BrukMak@users.noreply.github.com>
2023-03-09 04:04:04 +0300

    Time: 35 ms (45.36%), Space: 13.9 MB (50.26%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
@@ -3,12 +3,9 @@ class Solution:
         if rowIndex == 0:
             return [1]
         
-        prev_level =  self.getRow(rowIndex - 1)
+        prev_level = [0] + self.getRow(rowIndex - 1)
         prev_level.append(0)
         res = []
-        for i in range(len(prev_level)):
-            if i == 0:
-                res.append(prev_level[i])
-            else:
-                res.append(prev_level[i-1] + prev_level[i])
+        for i in range(1, len(prev_level)):
+            res.append(prev_level[i-1] + prev_level[i])
         return res
