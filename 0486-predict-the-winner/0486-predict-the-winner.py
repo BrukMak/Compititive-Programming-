eaf6c8186f044b6e51f76b183dc8375a06218c23
BrukMak <60716114+BrukMak@users.noreply.github.com>
2023-03-16 00:12:28 +0300

    Time: 8451 ms (5.13%), Space: 13.8 MB (98.19%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.py
+++ 0486-predict-the-winner/0486-predict-the-winner.py
@@ -1,20 +1,13 @@
 class Solution:
     def PredictTheWinner(self, nums: List[int]) -> bool:
         N = len(nums)
-        def helper(i, j, p1_sum, p2_sum, turn):
+        def helper(i, j):
             if i > j:
-                return p1_sum >= p2_sum
+                return 0
             
-            if turn:
-                left = helper(i+1, j, p1_sum + nums[i], p2_sum, not turn)
-                right = helper(i, j-1, p1_sum + nums[j], p2_sum, not turn)
-                return left or right
-            else:
-                left = helper(i+1, j, p1_sum , p2_sum + nums[i], not turn)
-                right = helper(i, j-1, p1_sum , p2_sum + nums[j], not turn)
-                return left and right
-                
-        return helper(0, N - 1, 0, 0, True)
+            return max(nums[i] - helper(i+1, j), nums[j] - helper(i, j-1))            
+            
+        return helper(0, N - 1) >= 0
     
         
         
