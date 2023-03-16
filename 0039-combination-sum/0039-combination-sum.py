88f769aa48802fbdf1f3febc6760d7b3776abaf7
BrukMak <60716114+BrukMak@users.noreply.github.com>
2023-03-16 10:45:24 +0300

    Time: 47 ms (95.96%), Space: 14.1 MB (22.36%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,17 +1,14 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         self.res = []
-        def uniqueComb(index, curr, curr_sum):
-            if index >= len(candidates):
-                if curr_sum == target:
-                    self.res.append(curr[:])
+        def uniqueComb(index, curr_sum, curr):
+            if curr_sum == target:
+                self.res.append(curr[:])
                 return 
-            if curr_sum + candidates[index] <= target:
-                curr.append(candidates[index])
-                curr_sum += candidates[index]
-                uniqueComb(index, curr, curr_sum)
-                curr_sum -= candidates[index]
-                curr.pop()
-            uniqueComb(index+ 1, curr, curr_sum)
-        uniqueComb(0, [], 0)
+            for i in range(index, len(candidates)):
+                if candidates[i] + curr_sum > target:
+                    break  
+                uniqueComb(i, curr_sum + candidates[i], curr + [candidates[i]])
+        candidates.sort()
+        uniqueComb(0, 0, [])
         return self.res
