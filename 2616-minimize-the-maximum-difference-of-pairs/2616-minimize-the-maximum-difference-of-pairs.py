b772706509233f9028bfbfa04d61346907c9a058
BrukMak <60716114+BrukMak@users.noreply.github.com>
2023-05-01 15:59:58 +0300

    Time: 1671 ms (17.54%), Space: 30.9 MB (6.34%) - LeetHub

--- 2616-minimize-the-maximum-difference-of-pairs/2616-minimize-the-maximum-difference-of-pairs.py
+++ 2616-minimize-the-maximum-difference-of-pairs/2616-minimize-the-maximum-difference-of-pairs.py
@@ -1,24 +1,31 @@
 class Solution:
     def minimizeMax(self, nums: List[int], p: int) -> int:
         nums.sort()
-        left = 0
-        right = max(nums) - min(nums)
-        
-        while left <= right:
-            mid = (right + left) // 2
-            if self.isPossible(mid, nums, p):
-                right = mid - 1
-            else:
-                left = mid + 1
-        return left
+        def possibleComb(val):
+            count = 0
+            itera = 0
+
+            while itera < len(nums) - 1:
+
+                if math.fabs(nums[itera] - nums[itera + 1]) <= val:
+                    count += 1
+                    itera += 2
+                else:
+                    itera += 1
+            if count >= p:
+                return True
+            return False
     
-    def isPossible(self, val, nums, p):
-        count = 0
-        i = 0
-        while i < (len(nums)-1):
-            if nums[i+1] - nums[i] <= val:
-                i += 2
-                count += 1
-            else:
-                i += 1
-        return count >= p
+        def BinarySearch(left , right):
+            while left <= right:
+                mid = (left + right) // 2
+                if possibleComb(mid):
+                    right = mid - 1
+                    best = mid
+                else:
+                    left = mid + 1
+            return best
+        left = 0
+        right = (nums[-1] - nums[0])
+        best = right
+        return BinarySearch(left, right)
