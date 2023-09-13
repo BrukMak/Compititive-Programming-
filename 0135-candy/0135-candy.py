aa5c951436521d3328fd2eb96bbc284b8f7046cc
BrukMak <60716114+BrukMak@users.noreply.github.com>
2023-09-13 23:16:15 +0300

    Time: 265 ms (5.04%), Space: 20.8 MB (5.95%) - LeetHub

---
+++ 0135-candy/0135-candy.py
@@ -0,0 +1,59 @@
+class Solution:
+    def candy(self, ratings: List[int]) -> int:
+        
+        n = len(ratings)
+        if n == 1: return n
+        minHeap = [(val, i) for i, val in enumerate(ratings)]
+        heapq.heapify(minHeap)
+        resArr = [float('inf')] * n
+        
+        while minHeap:
+            val, i = heapq.heappop(minHeap)
+            print(val, i)
+            if i > 0 and i < n-1:
+                if resArr[i-1] == float('inf') and resArr[i+1] == float('inf'):
+                    resArr[i] = 1
+                elif resArr[i-1] != float('inf') and resArr[i+1] != float('inf'):
+                    if ratings[i-1] == val and ratings[i+1] == val:
+                        resArr[i] = 1
+                    elif ratings[i-1] == val:
+                        resArr[i] = resArr[i+1] + 1
+                    elif ratings[i+1] == val:
+                        resArr[i] = resArr[i-1] + 1
+                    else:
+                        resArr[i] = max(resArr[i-1], resArr[i+1]) + 1
+                elif  resArr[i-1] != float('inf'):
+                    if ratings[i-1] == val:
+                        resArr[i] = 1
+                    else:
+                        resArr[i] = resArr[i-1] + 1
+                
+                elif resArr[i+1] != float('inf'):
+                    if ratings[i+1] == val:
+                        resArr[i] = 1
+                    else:
+                        resArr[i] = resArr[i+1] + 1
+            elif i == 0 and i < n-1:
+                if resArr[i+1] == float('inf'):
+                    resArr[i] = 1
+                elif resArr[i+1] != float('inf'):
+                    if ratings[i+1] == val:
+                        resArr[i] = 1
+                    else:
+                        resArr[i] = resArr[i+1] + 1
+                    
+            elif i == n-1 and i > 0: 
+                if resArr[i-1] == float('inf'):
+                    resArr[i] = 1
+                elif resArr[i-1] != float('inf'):
+                    if ratings[i-1] == val:
+                        resArr[i] = 1
+                    else:
+                        resArr[i] = resArr[i-1] + 1
+        # print(resArr)
+        return sum(resArr)
+                        
+                        
+                        
+                        
+                
