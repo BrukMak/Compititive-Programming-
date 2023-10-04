245c87616b3f0ef4b5d55928e5be9f72c8f685f9
BrukMak <60716114+BrukMak@users.noreply.github.com>
2023-10-05 00:39:15 +0300

    Time: 74 ms (55.36%), Space: 17.5 MB (64.87%) - LeetHub

--- 0174-dungeon-game/0174-dungeon-game.py
+++ 0174-dungeon-game/0174-dungeon-game.py
@@ -3,30 +3,17 @@ class Solution:
         
         n = len(dungeon)
         m = len(dungeon[0])
-#         @cache
-#         def dp(i, j, runningSum, pathMin):
-#             if i == n-1 and j == m-1:
-#                 runningSum += dungeon[i][j]
-#                 pathMin = min(pathMin, runningSum)
-#                 return abs(pathMin) + 1
-#             if i >= n or j >= m:
-#                 return float('inf')
-            
-#             right = dp(i, j+1, runningSum + dungeon[i][j], min(pathMin, runningSum + dungeon[i][j]))
-#             down = dp(i + 1, j, runningSum + dungeon[i][j], min(pathMin, runningSum + dungeon[i][j]))
-#             return min(right, down)
-
-#         return dp(0, 0, 0, 0)
-
-        dp = [[float('-inf')] * (m+1) for _ in range(n+1)]
-
+        
         for i in range(n-1, -1, -1):
             for j in range(m-1, -1, -1):
                 if i == n-1 and j == m-1:
-                    dp[i][j] = min(0, dungeon[i][j])
+                    dungeon[i][j] = min(0, dungeon[i][j])
+                elif i == n-1:
+                    dungeon[i][j] = min(0, dungeon[i][j] + dungeon[i][j+1])
+                elif j == m-1:
+                    dungeon[i][j] = min(0, dungeon[i][j] + dungeon[i+1][j])
                 else:
-                    dp[i][j] = min(0, max(dp[i+1][j], dp[i][j+1]) + dungeon[i][j])
                     
-        return abs(dp[0][0]) + 1
-                
-        
+                    dungeon[i][j] = min(0, dungeon[i][j] + max(dungeon[i+1][j], dungeon[i][j+1]))
+
+        return abs(dungeon[0][0]) + 1
