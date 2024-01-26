386ab526ab0473ec3d649ce44decefb5edade846
BrukMak <60716114+BrukMak@users.noreply.github.com>
2024-01-26 19:25:34 +0300

    Time: 78 ms (33.79%), Space: 17.1 MB (52.86%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.py
+++ 1094-car-pooling/1094-car-pooling.py
@@ -1,12 +1,18 @@
 class Solution:
     def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
     
-        max_cap = defaultdict(int)
+        prefix_sum = [0] * 1001
         for trip in trips:
             num_pass, start, end  = trip
+            prefix_sum[start] += num_pass
+            prefix_sum[end] += -num_pass
             
-            for i in range(start, end):
-                max_cap[i] += num_pass
-        return max(max_cap.values()) <= capacity
-                
+        max_cap = prefix_sum[0]
+        for i in range(1, len(prefix_sum)):
+            prefix_sum[i] += prefix_sum[i-1]
+            max_cap = max(max_cap, prefix_sum[i])
+        return max_cap <= capacity
+        
+            
+            
                 
